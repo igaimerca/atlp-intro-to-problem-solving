758519d385b884342f7a9785083e3749f6229479
igaimerca <igaimerca@gmail.com>
2022-03-31 14:25:12 +0200

    feat: descending order

---
+++ sorting/descending.js
@@ -0,0 +1,18 @@
+let numbers = [2, 4, 1, 3, 5, 6];
+
+let sort = (arr) => {
+    let len = arr.length;
+    for (let i = 0; i < len; i++) {
+        for (let j = 0; j < len - 1; j++) {
+            if (arr[j] < arr[j + 1]) {
+                let tmp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = tmp;
+            }
+        }
+    }
+    return arr;
+};
+
+let ordered = sort(numbers);
+console.log(ordered);
